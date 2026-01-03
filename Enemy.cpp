c4a8b23e2acb58597f3de9c9f271b3820afb17a1
LaiLaoLanAN <3279664319@qq.com>
2026-01-03 21:16:42 +0800

    Add files via upload

---
+++ Enemy.cpp
@@ -0,0 +1,38 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Enemy {
+    char name[20];
+    int damage;
+    int mp;
+    struct Enemy* next;
+};
+
+struct Enemy* createEnemy(char* name, int damage, int mp) {
+    struct Enemy* newEnemy = (struct Enemy*)malloc(sizeof(struct Enemy));
+    strcpy(newEnemy->name, name);
+    newEnemy->damage = damage;
+    newEnemy->mp = mp;
+    newEnemy->next = NULL;
+    return newEnemy;
+}
+
+void printEnemies(struct Enemy* head) {
+    struct Enemy* current = head;
+    while (current != NULL) {
+        printf("Name: %s, Damage: %d, MP: %d\n",
+            current->name, current->damage, current->mp);
+        current = current->next;
+    }
+}
+
+int main() {
+    struct Enemy* head = createEnemy("Goblin", 10, 5);
+    head->next = createEnemy("Orc", 25, 15);
+    head->next->next = createEnemy("Dragon", 50, 30);
+
+    printEnemies(head);
+
+    return 0;
+}
